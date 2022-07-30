c9c297e81a36862af1cd899b1ba64735d555dfd4
pmjn1025 <pmjn1025@naver.com>
2022-07-30 13:04:30 +0900

    바닐라 데이터 추가

---
+++ src/main/java/com/sparta/jwt_refresh_token_include/JwtRefreshTokenIncludeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jwt_refresh_token_include;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRefreshTokenIncludeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRefreshTokenIncludeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/jwt_refresh_token_include/JwtRefreshTokenIncludeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jwt_refresh_token_include;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRefreshTokenIncludeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
